3d90bd76e80c6a8c123023b60033b186020feb4c
Diego <diego.luz.braga@gmail.com>
2020-03-01 14:44:12 -0300

    Updates

---
+++ precache-manifest.68a4fd90304dbca3d57513670e567e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe64c2776831b2c9c6dd366b013b078",
+    "url": "/RichTextEditor/index.html"
+  },
+  {
+    "revision": "4984828df97ccdb438f1",
+    "url": "/RichTextEditor/static/css/main.ad3ac356.chunk.css"
+  },
+  {
+    "revision": "5fc886d9debed9e00cec",
+    "url": "/RichTextEditor/static/js/2.bf802840.chunk.js"
+  },
+  {
+    "revision": "906df1d725cf70c4d01ebff619791290",
+    "url": "/RichTextEditor/static/js/2.bf802840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4984828df97ccdb438f1",
+    "url": "/RichTextEditor/static/js/main.ab7bdfe0.chunk.js"
+  },
+  {
+    "revision": "7d6286023db448775cb2",
+    "url": "/RichTextEditor/static/js/runtime-main.fd8b73c3.js"
+  }
+]);
