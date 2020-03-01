225c75e02bfdcf10c2c48f5a9f3c9934015a2eb2
Diego <diego.luz.braga@gmail.com>
2020-03-01 16:08:33 -0300

    feature: error message for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.scss";
 import { TextEditor } from "./components/TextEditor/TextEditor";
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor />
-    </div>
+    <Fragment>
+      <div className="App">
+        <TextEditor />
+      </div>
+      <div className="mobileMessage">
+        <h1>Ops!</h1>
+        <p>
+          Looks like you're trying to access from a mobile device! Sorry but we
+          recommend access from a desktop for a better experience!
+        </p>
+      </div>
+    </Fragment>
   );
 }
 
